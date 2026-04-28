4c4e72464e93dd607333e46cf02b1703f4634762
paulveloz28 <paulveloz28@gmail.com>
2026-04-28 14:49:22 -0500

    feat: modify the pintarTarjetas function and add a text box for the entering the salto

--- tarjetas.js
+++ tarjetas.js
@@ -10,4 +10,16 @@ function crearTarjetas() {
     //divTarjetas.innerHTML="<h1>MODIFICANDO DIV</h1>"
 }
 
+function pintarTarjetas() {
+    let contenido = " ";
+    let desde = parseInt(document.getElementById("txtDesde").value);
+    let hasta = parseInt(document.getElementById("txtHasta").value);
+    let salto = parseInt(document.getElementById("txtSalto").value);
 
+    let divTarjetas = document.getElementById("divTarjetas");
+    for (i = desde; i <= hasta; i += salto) {
+        contenido = contenido + "<div class='item'>" + i + "</div>";
+    }
+    divTarjetas.innerHTML = contenido;
+
+}
